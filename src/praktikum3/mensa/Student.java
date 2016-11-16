3986d510295775775caab1f4523efa287a1f4541
schorzz <sir.lightness@gmx.de>
2016-11-16 16:33:34 +0100

    Neuer Branch, neue Klasse Student, initial-Commit

---
+++ src/praktikum3/mensa/Student.java
@@ -0,0 +1,15 @@
+package praktikum3.mensa;
+
+public class Student extends Thread {
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+
+}
